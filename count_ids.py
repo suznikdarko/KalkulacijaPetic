aa2bbf5f105db5d39a621f88adc990897e361d58
Darko <suznikdarko@gmail.com>
2026-09-01 14:11:01 +0200

    izbrisano

--- count_ids.py
+++
@@ -1,7 +0,0 @@
-with open('blok.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-count1 = text.count('id="res-price-total"')
-count2 = text.count('id="res-price-per-item"')
-print('res-price-total count:', count1)
-print('res-price-per-item count:', count2)
